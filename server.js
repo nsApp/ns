334718c1a875f08bc0b6a95b0fad3f6bf4f50dba
Leow Zheng Yu <leowzhengyu@hotmail.com>
2017-12-01 00:25:04 -0500

    add route

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var express  = require('express');
-var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport	  = require('passport');
+var port        = process.env.PORT || 8080;
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var cors = require('cors');
+var cors        = require('cors');
 
 // Configuration
-mongoose.connect('mongodb://localhost/reviewking');
+mongoose.connect('mongodb://localhost/');
 
+// get our request parameters
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
@@ -16,23 +19,23 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// Use the passport package in our application
+app.use(passport.initialize());
+
+// Route (GET http://localhost:8080)
+// app.get('/', function(req, res) {
+//   res.send('Hello! The API is at http://localhost:' + port + '/api');
+// });
+
+app.get('/welcome', function (req,res) {
+  res.sendFile(__dirname + '/src/pages/welcome/welcome.html');
 });
 
-// Models
-var Review = mongoose.model('Review', {
-  title: String,
-  description: String,
-  rating: Number
+app.get('/signup', function (req,res) {
+  res.sendFile(__dirname + '/src/pages/signup/signup.html')
 });
-
-// Routes
 
 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
+app.listen(port);
 console.log("App listening on port 8080");
